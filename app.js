98dacc4751492e15ca105222d9536654b41bab05
Anmol Bhardwaj <anmolbhardwaj17@gmail.com>
2022-04-04 19:49:07 +0530

    connection updated

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const app = express();
+const dotenv = require('dotenv');
+dotenv.config({path: './config.env'});
+
+require('./db/connection');
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send("Initialize");
